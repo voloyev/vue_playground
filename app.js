4493003276667fe723f870394115d5231c8b3f1d
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-05-16 17:26:00 +0300

    Add broken package

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ new Vue({
   el: "#app",
   data: {
     attachedRed: false
+  },
+  computed: {
+    divClasses: function() {
+      return {
+        red: this.attachRed,
+        blue: !this.attachRed
+      };
+    }
   }
 });

566493e0377f892ec1f52f88668382642e5e089c
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-12-01 23:46:04 +0200

    add new assignment

---
+++ s/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <header-cmp></header-cmp>
+    <hr>
+    <div class="row">
+      <server-cmp></server-cmp>
+      <server-details></server-details>
+    </div>
+    <hr>
+    <footer-cmp></footer-cmp>
+  </div>
+</template>
+<script>
+import Servers from "./components/Servers/Server.vue";
+import ServerDetails from "./components/Servers/ServerDetails.vue";
+import Header from "./components/Shared/Header.vue";
+import Footer from "./components/SharedFooter.vue";
+
+export default {
+  components: {
+    "header-cmp": Header,
+    "server-cmp": Server,
+    "server-details": ServerDetails,
+    "footer-cmp": Footer
+  }
+};
+</script>

36e786724a566f4c970292320cfc6c6f315dfec7
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-12-02 21:36:23 +0200

    Fix

--- comm_comp/src/components/UserDetail.vue
+++ comm_comp/src/components/UserDetail.vue
@@ -2,13 +2,21 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name: {{name}}</p>
+        <p>User Name: {{ switchName }}</p>
     </div>
 </template>
 
 <script>
 export default {
-  props: ["name"]
+  props: ["myName"],
+  methods: {
+    switchName() {
+      this.myName
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
 };
 </script>
 
